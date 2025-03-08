ddad75518673375b5e6def4465b06d522dc8d790
Fahad mehfooz <fahiii1211@gmail.com>
2025-03-08 00:27:32 -0500

    Changed plotting

--- Modules/Pseudotime_Computation_Supervised.R
+++ Modules/Pseudotime_Computation_Supervised.R
@@ -6,8 +6,6 @@ library(grid)
 # Working directory needs to be ->/Supervised-Longitudinal-Progressive-Embedding/Modules
 # Ensure you follow our structure
 # Set working directory
-
-
 standard_theme <- theme_minimal() + 
   theme(
     plot.title = element_text(hjust = 0.5, size = 8, face = "bold"),
@@ -26,10 +24,11 @@ standard_theme <- theme_minimal() +
     plot.margin = margin(10, 10, 10, 10, "pt")
   )
 
-
-generate_model_boxplot <- function(data, model_type, dataset_type) {
+generate_model_boxplot <- function(data, model_type, dataset_type, show_x_title = FALSE) {
   my_colors <- c("CN" = "#F8766D", "EMCI" = "#7CAE00", "LMCI" = "#00BFC4", "AD" = "#C77CFF")
   my_comparisons <- list(c("CN", "EMCI"), c("EMCI", "LMCI"), c("LMCI", "AD"))
+  
+  x_title <- if(show_x_title) "Diagnosis Groups" else NULL
   
   ggboxplot(data, x = "DX", y = "Pseudotime_Normalized",
             fill = "DX", outlier.size = 0.001) +
@@ -37,18 +36,17 @@ generate_model_boxplot <- function(data, model_type, dataset_type) {
                        aes(label = ..p.signif..),
                        method = "t.test", size = 3.5,
                        step.increase = 0.15, tip.length = 0.01) +
-    labs(title = paste0( model_type), 
-         y = NULL, x = "Diagnosis Groups") + 
+    labs(title = paste0(model_type), 
+         y = NULL, x = x_title) + 
     scale_y_continuous(expand = expansion(mult = c(0, 0.15))) +
     standard_theme +
     theme(
       panel.grid = element_blank(),
-      axis.text.x = element_text(color = "black", size = 8, face = "bold", angle = 45),
+      axis.text.x = element_text(color = "black", size = 8, face = "bold", angle = 45, hjust = 1),
       axis.text.y = element_text(color = "black", size = 8, face = "bold"),
       plot.margin = margin(10, 10, 10, 10, "pt"),
       legend.position = "none"
     )
-  
 }
 
 plot_objects <- list(
@@ -68,9 +66,11 @@ combined_train <- ggarrange(
   labels = c("(a)", "(b)", "(c)"),
   font.label = list(size = 8, face = "bold"),
   legend = "none"
-) %>% annotate_figure(left = textGrob("Pseudotime", rot = 90, 
-                                      gp = gpar(fontsize = 8, fontface = "bold")))
-
+) %>% 
+  annotate_figure(
+    left = textGrob("Pseudotime", rot = 90, gp = gpar(fontsize = 8, fontface = "bold")),
+    bottom = textGrob("Diagnosis Groups", gp = gpar(fontsize = 8, fontface = "bold"))
+  )
 
 combined_test <- ggarrange(
   plot_objects$test_lr,
@@ -80,11 +80,13 @@ combined_test <- ggarrange(
   labels = c("(a)", "(b)", "(c)"),
   font.label = list(size = 8, face = "bold"),
   legend = "none"
-) %>% annotate_figure(left = textGrob("Pseudotime", rot = 90,
-                                      gp = gpar(fontsize = 10, fontface = "bold")))
+) %>% 
+  annotate_figure(
+    left = textGrob("Pseudotime", rot = 90, gp = gpar(fontsize = 10, fontface = "bold")),
+    bottom = textGrob("Diagnosis Groups", gp = gpar(fontsize = 8, fontface = "bold"))
+  )
 
 ggsave(file.path(output_dir, "Supervised_train_plots.png"), combined_train,
        width = 5, height = 2, dpi = 300, bg = "white")
-
 ggsave(file.path(output_dir, "Supervised_test_plots.png"), combined_test,
        width = 5, height = 2, dpi = 300, bg = "white")
