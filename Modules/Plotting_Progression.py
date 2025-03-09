94bc2774af6b838bc19ecb9dfabbe73ad18eb1f7
Fahad mehfooz <fahiii1211@gmail.com>
2025-03-09 18:20:34 -0400

    new changes

--- Modules/Plotting_Progression.py
+++ Modules/Plotting_Progression.py
@@ -54,51 +54,50 @@ def plot_embedding_results(filename, title="Image", figsize = (8, 6)):
 def plot_progression_all_models(figures_and_axes):
     fig = plt.figure(figsize=(18, 10))
     
-    ax1 = plt.subplot2grid((2, 6), (0, 1), colspan=2)  
-    ax2 = plt.subplot2grid((2, 6), (0, 3), colspan=2)  
+    ax1 = plt.subplot2grid((2, 6), (0, 1), colspan=2)
+    ax2 = plt.subplot2grid((2, 6), (0, 3), colspan=2)
     
-    ax3 = plt.subplot2grid((2, 6), (1, 0), colspan=2)  
-    ax4 = plt.subplot2grid((2, 6), (1, 2), colspan=2)  
-    ax5 = plt.subplot2grid((2, 6), (1, 4), colspan=2)  
+    ax3 = plt.subplot2grid((2, 6), (1, 0), colspan=2)
+    ax4 = plt.subplot2grid((2, 6), (1, 2), colspan=2)
+    ax5 = plt.subplot2grid((2, 6), (1, 4), colspan=2)
     
     SLOPE_fig, SLOPE_ax = figures_and_axes[0]
-
     for line in SLOPE_ax.lines:
         ax1.plot(line.get_xdata(), line.get_ydata(), color=line.get_color(), marker=line.get_marker())
-    ax1.set_xlabel('Age')
-    ax1.set_ylabel('Pseudotime')
+    ax1.set_xlabel('Age', fontweight='bold')
+    ax1.set_ylabel('Pseudotime', fontweight='bold')
     ax1.set_title('SLOPE')
     
     Autoencoder_fig, Autoencoder_ax = figures_and_axes[1]
     for line in Autoencoder_ax.lines:
         ax2.plot(line.get_xdata(), line.get_ydata(), color=line.get_color(), marker=line.get_marker())
-    ax2.set_xlabel('Age')
-    ax2.set_ylabel('Pseudotime')
+    ax2.set_xlabel('Age', fontweight='bold')
+    ax2.set_ylabel('Pseudotime', fontweight='bold')
     ax2.set_title('Autoencoder')
     
     mlp_fig, mlp_ax = figures_and_axes[2]
     for line in mlp_ax.lines:
         ax3.plot(line.get_xdata(), line.get_ydata(), color=line.get_color(), marker=line.get_marker())
     ax3.set_title('Logistic Regression')
-    ax3.set_xlabel('Age')
-    ax3.set_ylabel('Logit')
+    ax3.set_xlabel('Age', fontweight='bold')
+    ax3.set_ylabel('Logit', fontweight='bold')
     
     en_fig, en_ax = figures_and_axes[3]
     for line in en_ax.lines:
         ax4.plot(line.get_xdata(), line.get_ydata(), color=line.get_color(), marker=line.get_marker())
     ax4.set_title('Elastic Net')
-    ax4.set_xlabel('Age')
-    ax4.set_ylabel('Logit')
+    ax4.set_xlabel('Age', fontweight='bold')
+    ax4.set_ylabel('Logit', fontweight='bold')
     
     lr_fig, lr_ax = figures_and_axes[4]
     for line in lr_ax.lines:
         ax5.plot(line.get_xdata(), line.get_ydata(), color=line.get_color(), marker=line.get_marker())
     ax5.set_title('MLP')
-    ax5.set_xlabel('Age')
-    ax5.set_ylabel('Logit')
+    ax5.set_xlabel('Age', fontweight='bold')
+    ax5.set_ylabel('Logit', fontweight='bold')
     
-    fig.delaxes(plt.subplot2grid((2, 6), (0, 0))) 
-    fig.delaxes(plt.subplot2grid((2, 6), (0, 5)))  
+    fig.delaxes(plt.subplot2grid((2, 6), (0, 0)))
+    fig.delaxes(plt.subplot2grid((2, 6), (0, 5)))
     
     fig.suptitle('Subject Progression over Age', fontsize=16, fontweight='bold', y=1.05, ha='center')
     
@@ -107,33 +106,31 @@ def plot_progression_all_models(figures_and_axes):
 
 
 def plot_violation_metrics(results, thresholds, figsize=(15, 8)):
-    fig, axs = plt.subplots(1, 2, figsize = figsize )
-
+    fig, axs = plt.subplots(1, 2, figsize=figsize)
     initial_threshold_ratios = [metrics['vio_ratios'][0] for metrics in results.values()]
     models = list(results.keys())
     colors = plt.cm.viridis(np.linspace(0, 1, len(models)))
-
+    
     # Plot 1: Violation Ratio vs Threshold for each model
     for name, metrics in results.items():
         axs[0].plot(thresholds, metrics['vio_ratios'], label=name, marker='o')
     axs[0].set_title('Violation Ratio vs Threshold')
-    axs[0].set_xlabel('Threshold')
-    axs[0].set_ylabel('Violation Ratio')
+    axs[0].set_xlabel('Threshold', fontweight='bold')
+    axs[0].set_ylabel('Violation Ratio', fontweight='bold')
     axs[0].legend()
     axs[0].grid(True)
-
+    
     # Plot 2: Violation Gap vs Threshold for each model
     for name, metrics in results.items():
         axs[1].plot(thresholds, metrics['vio_gaps'], label=name, marker='o')
     axs[1].set_title('Violation Gap vs Threshold')
-    axs[1].set_xlabel('Threshold')
-    axs[1].set_ylabel('Violation Gap')
+    axs[1].set_xlabel('Threshold', fontweight='bold')
+    axs[1].set_ylabel('Violation Gap', fontweight='bold')
     axs[1].legend()
     axs[1].grid(True)
-
+    
     plt.tight_layout()
     plt.show()
-
 
 def plot_umap_embeddings(train_data, test_data, y_train, y_test, title):
 
